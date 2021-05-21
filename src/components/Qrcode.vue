e74ef0c8970bec4f087b6f69eea3f2eda158f9ce
kaJloyn <68390461+kaJloyn@users.noreply.github.com>
2021-05-21 15:57:19 +0300

    qr again

--- src/components/Qrcode.vue
+++ src/components/Qrcode.vue
@@ -1,7 +1,14 @@
 <template>
     <div>
-        <p>QR SCAN</p>
-        <qrcode-stream @decode="onDecode"></qrcode-stream>
+        <p class="decode-result">Last result: <b>{{ result }}</b></p>
+
+        <qrcode-stream :camera="camera" @decode="onDecode" @init="onInit">
+            <div v-show="cameraForzen" class="validation-layer">
+                <div class="validation-notice" v-if="validating">
+                    Long validation in progress...
+                </div>
+            </div>
+        </qrcode-stream>
     </div>
 </template>
 
@@ -14,11 +21,17 @@ export default {
     components: {
         QrcodeStream,
     },
-    methods: {
-        onDecode (decodedString) {
-            console.log(decodedString)
+    methods:{
+        async onDecode (content) {
+            this.result = content
+
+            this.stopCamera()
+            window.setTimeout(() => {
+                this.startCamera()
+            }, 2000)
         }
-    }
+    },
+
 }
 </script>
 
