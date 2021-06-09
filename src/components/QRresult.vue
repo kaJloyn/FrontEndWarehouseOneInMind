403aaa0a719abccc53330a5be99b6f59907003db
kaJloyn <68390461+kaJloyn@users.noreply.github.com>
2021-06-09 22:54:12 +0300

    some

--- src/components/QRresult.vue
+++ src/components/QRresult.vue
@@ -1,13 +1,32 @@
 <template>
-    <p>
-        {{this.$route.params}}
-    </p>
+    <div>
+        <div>
+            <img class="picture-rm" :src="pic" alt="">
+            <article>
+                <span>{{rm}}</span>
+                <span>{{size}}</span>
+                span{{color}}
+            </article>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "QRresult",
-
+        data(){
+            return {
+                pic:null,
+                rm: null,
+                size:null,
+                color:null,
+            }
+        },
+        created() {
+            this.pic = this.$route.params['result']['pic']
+            this.rm = this.$route.params['result']['title']
+            this.size = this.$route.params['result']['size']
+            this.color = this.$route.params['result']['color']
+        }
     }
 </script>
 
--- src/components/Qrcode.vue
+++ src/components/Qrcode.vue
@@ -41,9 +41,11 @@
                 if (content){
                     this.color = 'green'
                     let current_raw = this.rawMaterials.find(item => item.id === Number(content))
-                    this.result = current_raw
+                    this.result = Number(current_raw)
                 }
-                this.$router.push({name:'QRresult', params:[this.result]})
+                this.$router.push({name:'QRresult', params:{
+                    result: this.result
+                    }})
                 this.pause()
 
                 // this.unpause()
